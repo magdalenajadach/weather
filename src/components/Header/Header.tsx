66451873c0f5350346af10abf0abc7101f043550
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-10-30 12:34:49 +0000

    Tailwind classes to align content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,10 +14,10 @@ export default function Header() {
         />
       </div>
       <div className="relative z-10 flex flex-col gap-6 items-center justify-center h-full">
-        <h1 className="text-white text-4xl sm:text-6xl font-bold">
+        <h1 className="text-white text-4xl sm:text-6xl font-bold p-4">
           Perfect weather for running
         </h1>
-        <p className='text-white gap-6 text-2xl sm:text-4xl font-bold'>Check the weather conditions for any location and run</p>
+        <p className='text-white gap-6 text-2xl sm:text-4xl font-bold p-4'>Check the weather conditions for any location and run</p>
         {/* <input className="rounded-lg mt-20 p-4 text-3xl border-2 border-bright-pink" type="text" id="name" name="name" required  placeholder="Your location" /> */}
       </div>
     </header>
--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -11,23 +11,26 @@ export default function Location() {
   // );
   // return <ul>{listItems}</ul>;
   return (
-    <div>
-      <h2 className="text-5xl text-bright-pink font-semibold">Sneak peak at best UK runnning locations</h2>
-      <div className="grid grid-cols-3 grid-rows-3 gap-4">
-        <div className="border-solid border-2 border-indigo-600">Grib Goch</div>
-        <div className="col-span-2 row-span-2 border-solid border-2 border-indigo-600">
-          <Image
-            src="/images/mud.jpg"
-            alt="Background"
-            width={200}
-            height={200}
-          />
+    <div className="w-full items-center min-h-screen">
+      <h2 className="text-4xl text-bright-pink font-semibold mb-12">Sneak peak at best UK runnning locations</h2>
+      <div className="md:grid grid-cols-3 grid-rows-3 gap-4">
+        <div className="border-solid border-2 border-indigo-600 place-content-center items-center p-8">Grib Goch</div>
+        <div className="col-span-2 row-span-2 border-solid border-2 border-indigo-600 p-8">
+          Crib Goch is described as a knife-edged arête in the Snowdonia National Park in Gwynedd, Wales. The name means `red ridge` in the Welsh language. The highest point on the arête is 923 metres (3,028 ft) above sea level. All routes which tackle Crib Goch are considered mountaineering routes in winter or scrambles in summer.
         </div>
-        <div className="row-span-3 row-start-2 border-solid border-2 border-indigo-600">I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice I am baby tilde green juice </div>
-        <div className="row-span-2 col-start-2 row-start-3 border-solid border-2 border-indigo-600">I am baby tilde green juice </div>
-        <div className="row-span-2 col-start-3 row-start-3 border-solid border-2 border-indigo-600">I am baby tilde green juice </div>
+        <div className="row-span-3 row-start-2 border-solid border-2 border-indigo-600 relative">
+          <div className="w-full h-full relative">
+            <Image
+              src="/images/crib-goch.png"
+              alt="Background"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        <div className="row-span-2 col-start-2 row-start-3 border-solid border-2 border-indigo-600">Location: North Wales, Eryri National Park</div>
+        <div className="row-span-2 col-start-3 row-start-3 border-solid border-2 border-indigo-600">Weather: rainy, foggy, ugly</div>
       </div>
-    
     </div>
   )
 }
