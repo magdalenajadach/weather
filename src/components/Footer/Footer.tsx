85821d12240cc49ec28b3dce1cf61176e52f06c9
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-10-31 12:09:38 +0000

    Footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return (
-    <p>Footer</p>
+    <div className="font-semibold flex w-full items-center justify-center min-h-screen sm:p-20 gap-3">
+      <a href="mailto:contact@jadach.tech" className="">contact@jadach.dev</a>
+      <p>Github</p>
+      <p>Strava</p>
+    </div>
   )
 }
