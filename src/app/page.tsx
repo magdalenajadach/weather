5983442ab7c579d7c97d81c59b10592f7a912d72
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-10-31 11:09:41 +0000

    White bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header/Header";
 import Location from "@/components/Location/Location";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="flex w-full items-center justify-center min-h-screen sm:p-20">
         <Location />
       </div>
+      <Footer />
     </main>
   );
 }
