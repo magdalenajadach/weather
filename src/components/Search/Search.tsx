5cd50ab4e5e7ac3ffe2f58a5de2bafc41200c62a
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-11-04 13:11:39 +0000

    Tweak to call to accept location as a parameter

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,31 @@
 // import Image from "next/image" 
+"use client";
+import React, { useState, useEffect } from "react";
+import { fetchWeatherData } from "../../api/weatherApi";
 
 export default function Search() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const [selectedLocation, setSelectedLocation] = useState("London")
+
+  const handleLocationChange = (e) => {
+    setSelectedLocation(e.target.value);
+  };
+
+  useEffect(() => {
+    const getWeatherData = async () => {
+      try {
+        const data = await fetchWeatherData(selectedLocation);
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    getWeatherData();
+  }, [selectedLocation]);
+
+
   return (
     <div className="md:flex flex-col w-full items-center justify-center min-h-screen sm:p-20">
       {/* <p>Welcome to the weather application. It is a tool designed to provide
@@ -13,27 +38,35 @@ export default function Search() {
       <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div className="flex items-center ps-3">
-                <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Kinder Scout, Dark Peak of the Derbyshire, England </label>
+                <input id="kinder-scout" type="radio"  onChange={handleLocationChange} value="London" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label htmlFor="kinder-scout" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Kinder Scout, Dark Peak of the Derbyshire, England </label>
             </div>
         </li>
         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div className="flex items-center ps-3">
-                <input id="horizontal-list-radio-id" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                <label htmlFor="horizontal-list-radio-id" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tryfan, Ogwen Valley, Eryri, Wales</label>
+                <input id="tryfan" type="radio"  onChange={handleLocationChange} value="Manchester" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label htmlFor="tryfan" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tryfan, Ogwen Valley, Eryri, Wales</label>
             </div>
         </li>
         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div className="flex items-center ps-3">
-                <input id="horizontal-list-radio-military" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                <label htmlFor="horizontal-list-radio-military" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mam Tor, High Peak of Derbyshire, England</label>
+                <input id="mam-tor" type="radio" onChange={handleLocationChange} value="Leeds" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                <label htmlFor="mam-tor" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mam Tor, High Peak of Derbyshire, England</label>
             </div>
         </li>
       </ul>
       <div>
-        <h2>Location: Kinder Scout</h2>
-        <p>Weather: grim, wet, cold, nice</p>
-      </div>
+        <h2>Location: {selectedLocation}</h2>
+        {weatherData ? (
+        <div>
+          <p>City: {weatherData.name}</p>
+          <p>Temperature: {weatherData.main.temp} °C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+        ) : (
+          <p>Loading weather data...</p>
+        )}
+        </div>
       {/* <div className="grid grid-cols-5 grid-rows-5 gap-4">
         <div className="col-span-2 row-span-2 bg-white rounded-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
